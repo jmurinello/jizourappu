f78a6b457dd22bd84dd66da433f3b71dfda0a15a
jmurinello <jm.murinello@gmail.com>
2018-12-14 06:49:39 +0900

    Create webpack rule for eslint

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -16,6 +16,12 @@ module.exports = options => ({
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
